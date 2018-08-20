db67751d207a2ef4fc22da4b9e3aae1e454daebc
Bahadır Zeren <36567337+bahadrzeren@users.noreply.github.com>
2018-08-20 18:29:41 +0100

    Writing PairDutyLegAggregaterTest class is completed.

--- src/main/java/org/heuros/rule/PairRuleContext.java
+++ src/main/java/org/heuros/rule/PairRuleContext.java
@@ -1,13 +1,17 @@
 package org.heuros.rule;
 
+import org.apache.log4j.Logger;
 import org.heuros.core.rule.AbstractRuleContext;
+import org.heuros.core.rule.AggregatorRuleContext;
 import org.heuros.core.rule.ConnectionCheckerRuleContext;
 import org.heuros.core.rule.ExtensibilityCheckerRuleContext;
 import org.heuros.core.rule.ValidatorRuleContext;
+import org.heuros.core.rule.inf.Aggregator;
 import org.heuros.core.rule.inf.ConnectionChecker;
 import org.heuros.core.rule.inf.ExtensibilityChecker;
 import org.heuros.core.rule.inf.Rule;
 import org.heuros.core.rule.inf.Validator;
+import org.heuros.core.rule.proxy.AggregatorProxy;
 import org.heuros.core.rule.proxy.ConnectionCheckerProxy;
 import org.heuros.core.rule.proxy.ExtensibilityCheckerProxy;
 import org.heuros.core.rule.proxy.ValidatorProxy;
@@ -16,16 +20,23 @@ import org.heuros.core.rule.repo.ExtensibilityCheckerRepository;
 import org.heuros.core.rule.repo.ValidatorRepository;
 import org.heuros.data.model.PairView;
 import org.heuros.data.model.DutyView;
+import org.heuros.data.model.Pair;
 import org.heuros.exception.RuleAnnotationIsMissing;
 
 public class PairRuleContext extends AbstractRuleContext
-								implements ConnectionCheckerRuleContext<PairView>,
+								implements AggregatorRuleContext<Pair, DutyView>,
+											ConnectionCheckerRuleContext<PairView>,
 											ExtensibilityCheckerRuleContext<PairView, DutyView>,
 											ValidatorRuleContext<PairView> {
+
+	private static Logger logger = Logger.getLogger(PairRuleContext.class);
+
+	protected Aggregator<Pair, DutyView> aggregatorImpl;
 	protected ConnectionCheckerRepository<PairView> connectionCheckerRepo = new ConnectionCheckerRepository<PairView>();
 	protected ExtensibilityCheckerRepository<PairView, DutyView> extensibilityCheckerRepo = new ExtensibilityCheckerRepository<PairView, DutyView>();
 	protected ValidatorRepository<PairView> validatorRepo = new ValidatorRepository<PairView>();
 
+	protected AggregatorProxy<Pair, DutyView> aggregatorProxy;
 	protected ConnectionCheckerProxy<PairView> connectionCheckerProxy = new ConnectionCheckerProxy<PairView>(this.connectionCheckerRepo);
 	protected ExtensibilityCheckerProxy<PairView, DutyView> extensibilityCheckerProxy = new ExtensibilityCheckerProxy<PairView, DutyView>(this.extensibilityCheckerRepo);
 	protected ValidatorProxy<PairView> validatorProxy = new ValidatorProxy<PairView>(this.validatorRepo);
@@ -33,12 +44,25 @@ public class PairRuleContext extends AbstractRuleContext
 	@SuppressWarnings("unchecked")
 	public PairRuleContext registerRule(Rule rule) throws RuleAnnotationIsMissing {
 		super.registerRule(rule);
+		if (rule.isImplemented(Aggregator.class))
+			this.registerAggregatorRule((Aggregator<Pair, DutyView>) rule);
 		if (rule.isImplemented(ConnectionChecker.class))
 			this.registerConnectionCheckerRule((ConnectionChecker<PairView>) rule);
 		if (rule.isImplemented(Validator.class))
 			this.registerValidatorRule((Validator<PairView>) rule);
 		if (rule.isImplemented(ExtensibilityChecker.class))
 			this.registerExtensibilityCheckerRule((ExtensibilityChecker<PairView, DutyView>) rule);
+		return this;
+	}
+
+	@Override
+	public AggregatorRuleContext<Pair, DutyView> registerAggregatorRule(Aggregator<Pair, DutyView> rule)
+			throws RuleAnnotationIsMissing {
+		if (this.aggregatorProxy == null) {
+			this.aggregatorImpl = rule;
+			this.aggregatorProxy = new AggregatorProxy<Pair, DutyView>(rule);
+		} else
+			logger.error("Rule impl is already registered!");
 		return this;
 	}
 
@@ -64,6 +88,11 @@ public class PairRuleContext extends AbstractRuleContext
 	}
 
 	@Override
+	public Aggregator<Pair, DutyView> getAggregatorImpl() {
+		return this.aggregatorImpl;
+	}
+
+	@Override
 	public ConnectionCheckerRepository<PairView> getConnectionCheckerRepo() {
 		return this.connectionCheckerRepo;
 	}
@@ -76,6 +105,11 @@ public class PairRuleContext extends AbstractRuleContext
 	@Override
 	public ExtensibilityCheckerRepository<PairView, DutyView> getExtensibilityCheckerRepo() {
 		return this.extensibilityCheckerRepo;
+	}
+
+	@Override
+	public AggregatorProxy<Pair, DutyView> getAggregatorProxy() {
+		return this.aggregatorProxy;
 	}
 
 	@Override
